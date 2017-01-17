d052259011452b6b220f9670a5054eac0b3e53e1
Jeremy England <Jeremy England>
2017-01-16 18:14:06 -0600

    more demos

--- test/demo.js
+++ test/demo.js
@@ -5,6 +5,7 @@ const {
 
 let demoLight;
 let demoDark;
+let demoCustom;
 
 app.on('ready', () => {
 
@@ -17,5 +18,10 @@ app.on('ready', () => {
     demoDark.setTitle('vertical demo');    
     demoDark.loadURL('file://' + __dirname + '/demo-dark.html');
     demoDark.on('close', () => {demoDark = null;});
+
+    demoCustom = new BrowserWindow();
+    demoCustom.setTitle('custom demo');    
+    demoCustom.loadURL('file://' + __dirname + '/demo-custom.html');
+    demoCustom.on('close', () => {demoCustom = null;});
 
 });
